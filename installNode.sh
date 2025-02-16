68e3f2f7751b0af845072e6214b999daf9b76f17
Ahmed El Bahri <bahri.tanger10@gmail.com>
2025-02-16 21:57:26 +0100

    Create installNode.sh

---
+++ installNode.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+V="23.8.0-linux-x64"
+
+rm -rf $HOME/.node
+rm -rf $HOME/goinfre/.node
+
+curl -fsSL https://nodejs.org/dist/latest/node-v$V.tar.xz -o $HOME/goinfre/node.tar.xz
+
+tar -xf $HOME/goinfre/node.tar.xz -C $HOME/goinfre/
+
+mv /$HOME/goinfre/node-v$V ~/goinfre/.node
+
+if ! grep -q 'export PATH="$HOME/goinfre/.node/bin:$PATH"' ~/.zshrc; then
+    echo 'export PATH="$HOME/goinfre/.node/bin:$PATH"' >> ~/.zshrc
+    echo "Added Node.js path to .zshrc"
+else
+    echo "Node.js path already exists in .zshrc"
+fi
+
+source ~/.zshrc
